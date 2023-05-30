5e0111290563ac47642f735c0a156c3dd0ceed70
CarloSalaris <carloa.salaris@gmail.com>
2023-05-30 16:15:55 +0200

    created + imported AppHero component (still empty)

---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: "AppHero"
+}
+</script>
+
+<template>
+    <div id="heroContainer">
+        Jumbotron goes here
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    color: #ffffff;
+}
+</style>
